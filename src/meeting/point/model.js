31645a7ca850a09a727e2f72e5226431cf08d7cd
Jerred Shepherd <shepherdjerred@gmail.com>
2018-04-19 11:28:57 -0500

    Add point model

---
+++ src/meeting/point/model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+let pointSchema = mongoose.Schema({
+  type: String,
+  coordinates: [Number]
+});
+
+let pointModel = mongoose.model('Point, pointSchema');
+
+module.exports = {
+  pointSchema,
+  pointModel
+};
