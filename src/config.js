25b12d81869ac433ed0d51aa0357dd1e9837f794
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-12-19 00:04:31 -0600

    Add docker file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-let mongoDbUrl = process.env.mongodb || 'mongodb://localhost:32768/huclubs';
-let frontEndUrl = process.env.frontEndUrl || 'http://localhost:3000';
+let mongoDbUrl = process.env.MONGO_DB_URL || 'mongodb://localhost:32768/huclubs';
+let frontEndUrl = process.env.FRONT_END_URL || 'http://localhost:3000';
 let port = process.env.PORT || 8080;
 let jwtSecret = process.env.JWT_SECRET || 'secret';
 let jwtIssuer = process.env.JWT_ISSUER || 'hu-clubs';
